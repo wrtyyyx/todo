f6360cad85495cad64b089dcc304e172ed5f7462
wrtyyyx <126886326+wrtyyyx@users.noreply.github.com>
2024-12-21 17:55:30 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import Header from "./components/Header/Header.jsx";
 import Form from "./components/Form/Form.jsx";
-import {putTodo} from './components/Card/renderCard.jsx'
+import {useState} from "react";
+import Card from "./components/Card/Card.jsx";
 import './App.css';
 
 function App() {
+    const [todos, setTodos] = useState([]);
+    const [nextId, setNextId] = useState(1)
+    const putTodo = (value) => {
+        if (value) {
+            setTodos([...todos, {id: nextId, text: value, done: false}]);
+            setNextId(nextId +1 )
+        } else {
+            alert('Enter task');
+        }
+    };
+
+    const removeTodo = (id) => {
+        setTodos(todos.filter(todo => todo.id !== id));
+    };
+
     return (
         <>
             <Header/>
@@ -11,7 +27,9 @@ function App() {
             <Form putTodo={putTodo}/>
             <div className={"todos_box"}>
                 {
-
+                    todos.map((card) => (
+                        <Card key={card.id} id={card.id} text={card.text} removeTodo={removeTodo}/>
+                    ))
                 }
             </div>
         </>
